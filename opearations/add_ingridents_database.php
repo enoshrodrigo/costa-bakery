9571df146357e0e3d91d883f6ea481491cbefa34
ENOSH RODRIGO <111069060+enoshrodrigo@users.noreply.github.com>
2023-08-01 12:32:04 +0530

    null problem

--- opearations/add_ingridents_database.php
+++ opearations/add_ingridents_database.php
@@ -9,13 +9,14 @@ if(isset($_POST['submit'])){
          
     ));
     if( $addIngredient->insertOData()){
+    $addIngredientValues = new Operation();
         //check ingredient_name and get ingredient_id from ingredient table 
-        $addIngredient->setTable('ingredient');
-        $addIngredient->setFields(array(
+        $addIngredientValues->setTable('ingredient');
+        $addIngredientValues->setFields(array(
             'ingredient_name' => $_POST['ingredient_name']
              
         ));
-        $ingredient_id = json_encode($addIngredient->selectOData('ingredient_name="'.$_POST['ingredient_name'].'"', 'ingredient_id'));
+        $ingredient_id = json_encode($addIngredientValues->selectOData('ingredient_name="'.$_POST['ingredient_name'].'"', 'ingredient_id'));
      $ingredient_id = json_decode($ingredient_id,true);
          
         // var_dump($ingredient_id);
@@ -27,15 +28,15 @@ if(isset($_POST['submit'])){
         //insert ingredient_id and other values to ingredientvalue table
 
 
-        $addIngredient->setTable('ingredientvalue');
-        $addIngredient->setFields(array(
+        $addIngredientValues->setTable('ingredientvalue');
+        $addIngredientValues->setFields(array(
             'ingredientvalue_ingredient_id' => $ingredient_id_array[0],
             'ingredientvalue_price' => $_POST['ingredientvalue_price'],
             'ingredientvalue_measure' => $_POST['ingredientvalue_measure'],
-            'ingredientvalue_date_start' => date('Y-m-d'),
-            'ingredientvalue_date_end' => $_POST['ingredientvalue_date_end']
+            'ingredientvalue_date_start' => date('Y-m-d')
         ));
-        if( $addIngredient->insertOData()){
+      
+        if( $addIngredientValues->insertOData()){
             header("Location: ../add_ingridents.php");
         }
         else{
